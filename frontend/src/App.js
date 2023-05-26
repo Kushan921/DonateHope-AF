cd72fa0e2e72dc7fbb35f4984bb363c2c0f59248
pramudithaSJ <IT20620202@my.sliit.lk>
2023-05-27 02:25:04 +0530

    psj frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 import Dashboard from "./IT20600884/dashboard";
 import AllDoctors from "./IT20600884/Doctor/AllDoctors";
@@ -12,8 +16,8 @@ import AllRequests from "./IT20603236/Requests/Requests";
 import BloodCount from "./IT20600884/BloodCount/BloodCount";
 import MyRequests from "./IT20603236/MyRequests/MyRequests";
 import DoctorLogin from "./IT20600884/Doctor/login";
-
-
+import UserDash from "./IT20620202/UserView/componenent/userDashboard";
+import DoctorSidebar from "./IT20620202/DoctorView/doctorDashbord";
 
 function App() {
   return (
@@ -22,20 +26,20 @@ function App() {
         <Routes>
           <Route path="/" exact element={<HomePage />} />
           <Route path="/login" exact element={<LoginUser />} />
-          <Route path="/loginsample" exact element={<LoginUser />} />
           <Route path="/signin" exact element={<SignUpUser />} />
+          <Route path="/myProfile" exact element={<UserDash />} />
+          <Route path="/doctorDashboard" exact element={<DoctorSidebar />} />
+
           <Route path="/dashboard" exact element={<Dashboard />} />
           <Route path="/doctors" exact element={<AllDoctors />} />
           <Route path="/masters" exact element={<AllMasters />} />
-          <Route path="/teachers02" exact element={<AllMasters />} />
-          <Route path="/students" exact element={<AllMasters />} />
-          <Route path="/teachers" exact element={<AllMasters />} />
-          <Route path="/user-dashboard" exact element={<Sidebar/>} />
-          <Route path="/requests" exact element={<AllRequests/>} />
+
+          <Route path="/user-dashboard" exact element={<Sidebar />} />
+          <Route path="/requests" exact element={<AllRequests />} />
           <Route path="/doctors" exact element={<AllDoctors />} />
-          <Route path="/blood-count" exact element={<BloodCount/>} />
-          <Route path="/my-requests" exact element={<MyRequests/>} /> 
-          <Route path="/doctor-login" exact element={<DoctorLogin/>} /> 
+          <Route path="/blood-count" exact element={<BloodCount />} />
+          <Route path="/my-requests" exact element={<MyRequests />} />
+          <Route path="/doctor-login" exact element={<DoctorLogin />} />
         </Routes>
       </BrowserRouter>
     </div>
